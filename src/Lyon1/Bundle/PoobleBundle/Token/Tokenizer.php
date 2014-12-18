18bb426199979500ebece482b1b82f792a64edc2
MaximeVeniant <maxime.veniant@gmail.com>
2014-12-18 16:16:08 +0100

    Creation du service Tokenizer

---
+++ src/Lyon1/Bundle/PoobleBundle/Token/Tokenizer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lyon1\Bundle\PoobleBundle\Token;
+
+use Lyon1\Bundle\PoobleBundle\Entity\Survey;
+
+class Tokenizer
+{
+    private $secret;
+
+    public function __construct($secret)
+    {        
+        $this->secret = $secret;
+    }
+
+    public function generateToken(Survey $survey)
+    {
+        $date = new \DateTime();        
+        return md5($this->secret.$date->format('Ymd').$survey->getName());
+    }
+}
